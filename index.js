def9ef6bdc760e1ec9b6acf1d444312c94a07dcf
Miguel Poulin <96082631+Migpoulin@users.noreply.github.com>
2023-11-10 15:37:19 -0500

    avancement

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
-app.use(express.json());
-app.use("/api/converter", require("./routes/api/converter"))
 
-app.get("/", (req, res) => {
 
-    res.send("Home sweet home");
+mongoose.connect('mongodb+srv://nodejs_projet:AQuuovw30OTgT9om@cluster0.8xuijmc.mongodb.net/journal', { useNewUrlParser: true, useUnifiedTopology: true });
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion :'));
+db.once('open', () => {
+    console.log('Connecté à MongoDB Atlas');
 });
 
 
-app.listen(80, () => console.log('Server started'));
+// Routes
+app.use(express.json());
+app.use("/api/converter", require("./routes/api/converter"));
+
+
+
+// Port d'écoute du serveur
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
+});

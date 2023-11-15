a01a071bfb7b499661e73fab7a473c812db8a484
Matias Beaulieu <104645036+matiasbeaulieu@users.noreply.github.com>
2023-11-15 15:25:24 -0500

    test

--- routes/api/converter.js
+++ routes/api/converter.js
@@ -33,7 +33,6 @@ router.use(async (req, res, next) => {
     }
 });
 
-
 router.get("/", (req, res) => {
     res.send("Need a POST request plz!");
 });
@@ -43,62 +42,62 @@ router.post("/", async (req, res) => {
     let resultat;
     let message;
     
-
-    switch (type.toLowerCase()) {
-        case "feet2meter":
-            resultat = parseFloat(value) * 0.3048;
-            unite = "mètres"
-            message = `${value} feet converted to ${resultat} meters.`;
-            break;
-        case "meter2feet":
-            resultat = parseFloat(value) / 0.3048;
-            unite = "pieds"
-            message = `${value} meters converted to ${resultat} feet.`;
-            break;
-        case "kilo2livre":
-            resultat = parseFloat(value) * 2.20462;
-            unite = "livres"
-            message = `${value} kilograms converted to ${resultat} pounds.`;
-            break;
-        case "livre2kilo":
-            resultat = parseFloat(value) / 2.20462;
-            unite = "Kilogrammes"
-            message = `${value} pounds converted to ${resultat} kilograms.`;
-            break;
-        case "celsius2fahrenheit":
-            resultat = (parseFloat(value) * 9/5) + 32;
-            unite = "fahrenheits"
-            message = `${value} degrees Celsius converted to ${resultat} degrees Fahrenheit.`;
-            break;
-        case "fahrenheit2celsius":
-            resultat = (parseFloat(value) - 32) * 5/9;
-            unite = "celsuis"
-            message = `${value} degrees Fahrenheit converted to ${resultat} degrees Celsius.`;
-            break;
-        case "litre2gallon":
-            resultat = parseFloat(value) * 0.264172;
-            unite ="gallons"
-            message = `${value} liters converted to ${resultat} gallons.`;
-            break;
-        case "gallon2litre":
-            resultat = parseFloat(value) * 3.78541;
-            unite ="litres"
-            message = `${value} gallons converted to ${resultat} liters.`;
-            break;
-        case "gravite2meterspersecondsquared":
-            resultat = parseFloat(value) * 9.80665;
-            unite = "m/s²"
-            message = `${value} g converted to ${resultat} m/s².`;
-            break;
-        case "meterspersecondsquared2gravite":
-            resultat = parseFloat(value) / 9.80665;
-            unite = "gravités"
-            message = `${value} m/s² converted to ${resultat} g.`;
-            break;
-        default:
-            res.status(400).send("Le type entrée est invalide.");
-            return;
-            
+    if (type !== undefined) {
+        switch (type.toLowerCase()) {
+            case "feet2meter":
+                resultat = parseFloat(value) * 0.3048;
+                unite = "mètres"
+                message = `${value} feet converted to ${resultat} meters.`;
+                break;
+            case "meter2feet":
+                resultat = parseFloat(value) / 0.3048;
+                unite = "pieds"
+                message = `${value} meters converted to ${resultat} feet.`;
+                break;
+            case "kilo2livre":
+                resultat = parseFloat(value) * 2.20462;
+                unite = "livres"
+                message = `${value} kilograms converted to ${resultat} pounds.`;
+                break;
+            case "livre2kilo":
+                resultat = parseFloat(value) / 2.20462;
+                unite = "Kilogrammes"
+                message = `${value} pounds converted to ${resultat} kilograms.`;
+                break;
+            case "celsius2fahrenheit":
+                resultat = (parseFloat(value) * 9/5) + 32;
+                unite = "fahrenheits"
+                message = `${value} degrees Celsius converted to ${resultat} degrees Fahrenheit.`;
+                break;
+            case "fahrenheit2celsius":
+                resultat = (parseFloat(value) - 32) * 5/9;
+                unite = "celsuis"
+                message = `${value} degrees Fahrenheit converted to ${resultat} degrees Celsius.`;
+                break;
+            case "litre2gallon":
+                resultat = parseFloat(value) * 0.264172;
+                unite ="gallons"
+                message = `${value} liters converted to ${resultat} gallons.`;
+                break;
+            case "gallon2litre":
+                resultat = parseFloat(value) * 3.78541;
+                unite ="litres"
+                message = `${value} gallons converted to ${resultat} liters.`;
+                break;
+            case "gravite2meterspersecondsquared":
+                resultat = parseFloat(value) * 9.80665;
+                unite = "m/s²"
+                message = `${value} g converted to ${resultat} m/s².`;
+                break;
+            case "meterspersecondsquared2gravite":
+                resultat = parseFloat(value) / 9.80665;
+                unite = "gravités"
+                message = `${value} m/s² converted to ${resultat} g.`;
+                break;
+            default:
+                res.status(400).send("Le type entrée est invalide.");
+                return; 
+        }
     }
 
     const accessLog = new AccessLog({
@@ -116,8 +115,6 @@ router.post("/", async (req, res) => {
         console.error('Erreur lors de l\'enregistrement du journal d\'accès :', error);
         //res.status(500).send('Erreur lors de l\'enregistrement du journal d\'accès');
     }
-    
-    
 
     console.log(`Données reçues : ${type} pour ${value}`);
     console.log(message);
